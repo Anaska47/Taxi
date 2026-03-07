eb88c5156dd31f54c8693a5862f0b57b251f8f21
Anaska47 <nskadiri@gmail.com>
2026-03-07 05:06:01 +0100

    chore: cleanup unused react files and logs

--- components/ConciergeAI.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ConciergeAI: React.FC = () => {
-    return null;
-};
-
-export default ConciergeAI;
--- services/imageService.ts
+++
@@ -1,1 +0,0 @@
-export const generateLuxuryImage = async (prompt?: string) => "";
